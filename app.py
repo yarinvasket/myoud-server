f680da30dca6b3a7d7d4412c7e294e40bd273144
vasker <yarinvasket@gmail.com>
2022-04-30 12:44:56 +0300

    logging

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+from base64 import encode
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 import random
 import logging
+
+logging.basicConfig(filename='log.log', encoding='utf-8', level=logging.DEBUG)
 
 app = Flask(__name__)
 api = Api(app)
