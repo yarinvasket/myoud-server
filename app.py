140d0d330f9ec2669633538d5422de46003c8e18
vasker <yarinvasket@gmail.com>
2022-04-30 15:53:42 +0300

    basic login and register requests (don't work yet)

--- app.py
+++ app.py
@@ -1,19 +1,41 @@
-from base64 import encode
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask import Flask, request
+from flask_restful import Api, Resource
 import random
 import logging
+
+from requests import session
 
 logging.basicConfig(filename='log.log', encoding='utf-8', level=logging.DEBUG)
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+class Register(Resource):
+    def post(self):
+        try:
+            user_name = request.form['user_name']
+            public_key = request.form['public_key']
+            signature = request.form['signature']
+            encrypted_private_key = request.form['encrypted_private_key']
+            hashed_password = request.form['hashed_password']
+        except Exception as e:
+            logging.error('Register formatting error: ' + str(e))
+            return {'message':'invalid format'}, 400
+        return 200
 
-api.add_resource(HelloWorld, '/api')
+class Login(Resource):
+    def post(self):
+        try:
+            user_name = request.form['user_name']
+            hashed_password = request.form['hashed_password']
+            session_timeout = request.form['session_timeout']
+        except Exception as e:
+            logging.error('Login formatting error: ' + str(e))
+            return {'message':'invalid format'}, 400
+        return 200
+
+
+api.add_resource(Register, '/api/register')
 
 if __name__ == '__main__':
     app.run(debug=True)
