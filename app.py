0a9ad916e39dabe95275cb623aa240f96f124169
vasker <yarinvasket@gmail.com>
2022-05-09 15:16:28 +0300

    use json

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import json
 import logging
 import sqlite3
 
 import bcrypt
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_response
 from flask_restful import Api, Resource
 
 from queries import *
@@ -36,14 +37,18 @@ class Register(Resource):
     def post(self):
 #       Get input from request and check that it is valid
         try:
-            user_name = request.form['user_name']
-            public_key = request.form['public_key']
-            signature = request.form['signature']
-            encrypted_private_key = request.form['encrypted_private_key']
-            hashed_password = request.form['hashed_password']
+            reqjson = request.json
+            user_name = reqjson['user_name']
+            public_key = reqjson['public_key']
+            signature = reqjson['signature']
+            encrypted_private_key = reqjson['encrypted_private_key']
+            hashed_password = reqjson['hashed_password']
         except Exception as e:
-            logging.error('Register formatting error: ' + str(e))
-            return {'message':'invalid format'}, 400
+            logging.error('Register formatting error: ' + str(e) +\
+                ' The format isnt json, or is missing a field.')
+#           TODO: DELETE THIS. POSSIBLE PASSWORD LOGGING
+            logging.error(request.json)
+            return make_response(jsonify(message="invalid format"), 400)
         
 #       Check that every field is valid
         try:
@@ -53,26 +58,27 @@ class Register(Resource):
             faultyString(encrypted_private_key)
             faultyString(hashed_password)
         except Exception as e:
-            logging.error('Register formatting error: ' + str(e))
-            return {'message':'invalid format'}, 400
+            logging.error('Register formatting error: ' + str(e) +\
+                ' One of the fields is a wrong type.')
+            return make_response(jsonify(message="invalid format"), 400)
 
 #       Check validity of the username
         if (len(user_name) > 16 or len(user_name) <= 0):
             logging.error('Register error: user name ' + user_name + ' is too long')
-            return {'message':'username is invalid'}, 400
+            return make_response(jsonify(message="username is invalid"), 400)
 
 #       Check that the username isn't taken
         db, cur = connect_db()
         cur.execute(get_user, {"username": user_name})
         if (cur.fetchall()):
             logging.error('Register error: user ' + user_name + " already exists")
-            return {'message':'username is taken'}, 400
+            return make_response(jsonify(message="username is taken"), 400)
 
 #       Check that the public key isn't taken
         cur.execute(get_user_by_pk, {"pk": public_key})
         if (cur.fetchall()):
             logging.error('Register error: public key' + public_key + ' already exists')
-            return {'message':'public key is taken'}, 400
+            return make_response(jsonify(message="public key is taken"), 400)
 
 #       TODO: check that the signature is valid
 
@@ -92,12 +98,13 @@ class Register(Resource):
 class Login(Resource):
     def post(self):
         try:
-            user_name = request.form['user_name']
-            hashed_password = request.form['hashed_password']
-            session_timeout = request.form['session_timeout']
+            reqjson = request.json
+            user_name = reqjson['user_name']
+            hashed_password = reqjson['hashed_password']
+            session_timeout = reqjson['session_timeout']
         except Exception as e:
             logging.error('Login formatting error: ' + str(e))
-            return {'message':'invalid format'}, 400
+            return make_response(jsonify(message='invalid format'), 400)
         
         try:
             faultyString(user_name)
@@ -105,7 +112,7 @@ class Login(Resource):
             faultyType(session_timeout, int)
         except Exception as e:
             logging.error('Login formatting error: ' + str(e))
-            return {'message':'invalid format'}, 400
+            return make_response(jsonify(message='invalid format'), 400)
         return 200
 
 api.add_resource(Register, '/api/register')
